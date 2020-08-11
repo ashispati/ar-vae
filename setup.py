b627cf1663b263537360af487b9a4fc310e70e30
Ashis Pati <ashis.pati@gatech.edu>
2020-08-11 10:37:17 -0400

    fix #1, add setup.py, move to setuptools based install

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name='arvae',
+    version='1.0',
+    description='Attrbute-based Regularization for Latent Spaces of VAEs',
+    author='Ashis Pati',
+    author_email='ashis.pati@gatech.edu',
+    packages=find_namespace_packages(),
+)
